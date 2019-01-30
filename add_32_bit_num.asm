2eb08ea416d61a0136490cdd267dfdc5316665d4
Harshith Kulkarni <harshithkulkarni@gmail.com>
2019-01-30 15:52:23 +0530

    Create add_32_bit_num.asm

---
+++ add_32_bit_num.asm
@@ -0,0 +1,23 @@
+.model small
+.stack 100
+.data
+        L1 DW 45F2H;
+        H1 DW 897BH;
+        L2 DW 32ABH;
+        H2 DW 123CH;
+        resL DW ?;
+        resH DW ?;
+.code
+        mov AX , @data;
+        mov DS , AX;
+        mov AX , L1;
+        mov BX , L2;
+        add AX , BX;
+        mov resL , AX;
+        mov AX , H1;
+        mov BX , H2;
+        adc AX , BX;
+        mov resH , AX;
+        mov AH , 4CH;
+        int 21H;
+end
